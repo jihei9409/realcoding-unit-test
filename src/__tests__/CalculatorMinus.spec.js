97e2e47d86cbc59718e2197b55d7000edb13c3f5
202002575 <eogkrry2020@gmail.com>
2022-05-02 17:04:06 +0900

    사칙연산 뺄셈 테스트

--- src/__tests__/CalculatorMinus.spec.js
+++ src/__tests__/CalculatorMinus.spec.js
@@ -2,6 +2,6 @@ import Calculator from "../js/calculator/Calculator";
 
 describe("사직연산 뺄셈 테스트.", () => {
     test("9에서 5를 빼면 4이다.", () => {
-        expect(9 - 5).toEqual(4);
+        expect(calculator.minus(9, 5)).toEqual(4);
     });
 });
