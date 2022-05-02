345bcf110c6d6bdfc64229b2d2134004ed19592e
jhYun505 <y_jh0505@naver.com>
2022-05-02 16:39:31 +0900

    Multiply Test

---
+++ src/__tests__/CalculatorMultiply.spec.js
@@ -0,0 +1,7 @@
+import Calculator from "../js/calculator/Calculator";
+
+describe("사칙연산 곱셈 테스트.", () => {
+  test("4와 5를 곱하면 20이다.", () => {
+    expect(calculator.multiply(4, 5)).toEqual(20);
+  });
+});
