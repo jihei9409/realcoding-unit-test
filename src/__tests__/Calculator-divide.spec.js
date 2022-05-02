890949add5ca80548688c1e6bb4d353f8ec1502a
JWG <jwg@MacBook-Pro.local>
2022-05-02 17:11:26 +0900

    final divide-test

--- src/__tests__/Calculator-divide.spec.js
+++ src/__tests__/Calculator-divide.spec.js
@@ -3,6 +3,6 @@ import Calculator from "../js/calculator/Calculator";
 describe("사직연산 나눗셈 테스트.", () => {
   test("100을 5로 나누면 20이다.", () => {
     const calculator = new Calculator();
-    expect(Calculator.divide(100, 5)).toEqual(20);
+    expect(calculator.divide(100, 5)).toEqual(20);
   });
 });
