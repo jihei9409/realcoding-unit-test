2ec48b20e094e6cedf27d6b84b0807c92e114495
jhYun505 <y_jh0505@naver.com>
2022-05-05 15:17:46 +0900

    Homework #1 - 음수, 양수 판단 / Date 클래스 판단하기

--- src/__tests__/201802120/201802120.spec.js
+++ src/__tests__/201802120/201802120.spec.js
@@ -3,7 +3,7 @@ import Calculator from "../../js/calculator/Calculator";
 describe("음수 양수 판단", () => {
   test("isNegative에 7을 넣으면 false를 반환한다.", () => {
     const calculator = new Calculator();
-    expect(calculator.isNegative(7)).toEqual(false);
+    expect(calculator.isNegative(7)).toBeFalsy();
   });
   test("isNegative에 -3을 넣으면 true를 반환한다.", () => {
     const calculator = new Calculator();
@@ -11,7 +11,7 @@ describe("음수 양수 판단", () => {
   });
   test("isNegative에 0을 넣으면 true가 아니다.", () => {
     const calculator = new Calculator();
-    expect(calculator.isNegative(0)).toBeFalsy();
+    expect(calculator.isNegative(0)).not;
   });
 });
 
