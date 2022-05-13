38cbc85ef9b4879c9df274d63206d9672c68a288
202002575 <eogkrry2020@gmail.com>
2022-05-13 18:08:39 +0900

    추가과제

--- realcoding-unit-test/src/__tests__/202002575.spec.js
+++ realcoding-unit-test/src/__tests__/202002575.spec.js
@@ -1,4 +1,4 @@
-import Calculator from "../../js/calculator/Calculator";
+import Calculator from "../js/calculator/Calculator";
 
 beforeAll(() => {
     console.log("모든 테스트를 실행하기 전 한번만 실행된다.");
@@ -24,31 +24,12 @@ describe("음수 양수 판단", () => {
     test("isNegative에 0을 넣으면 true가 아니다.", () => {
         const calculator = new Calculator();
         expect(calculator.isNegative(0)).not.toBeTruthy();
+        false;
     });
 });
 describe("Date 클래스 판단하기", () => {
     test("getDate를 하면 Date객체가 나온다.", () => {
         const calculator = new Calculator();
         expect(calculator.getDate()).toBeInstanceOf(Date);
-    });
-});
-describe("가상 함수 테스트해보기", () => {
-    test("customCalculation는 전달 된 함수를 호출시킨다.", () => {
-        const calculator = new Calculator();
-        const mockFunction = jest.fn();
-        calculator.customCalculation(mockFunction);
-        expect(mockFunction).toHaveBeenCalled();
-    });
-    test("customCalculation에 함수와 1, 2, 3인자를 전달하면, 전달 된 함수가 인자 1, 2, 3을 받아 호출한다.", () => {
-        const calculator = new Calculator();
-        const mockFunction = jest.fn();
-    calculator.customCalculation(mockFunction, 1, 2, 3);
-    expect(mockFunction).toHaveBeenCalledWith(1, 2, 3);
-    });
-    test("abs함수는 Math.abs를 호출한다.", () => {
-        const calculator = new Calculator();
-        const mockMathAbs = jest.spyOn(Math, 'abs');
-        calculator.abs(3);
-        expect(mockMathAbs).toHaveBeenCalled();
     });
 });
